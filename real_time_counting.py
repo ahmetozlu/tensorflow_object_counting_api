167a08678980d2834ce560b459ba586453fa9c59
souvik bera <44033278+berasouvik@users.noreply.github.com>
2018-10-10 22:22:51 +0530

    Update real_time_counting.py

--- real_time_counting.py
+++ real_time_counting.py
@@ -17,7 +17,7 @@ if tf.__version__ < '1.4.0':
 input_video = "New Office TOUR!  Karlie Kloss.mp4"
 
 # By default I use an "SSD with Mobilenet" model here. See the detection model zoo (https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/detection_model_zoo.md) for a list of other models that can be run out-of-the-box with varying speeds and accuracies.
-detection_graph, category_index = backbone.set_model('ssd_mobilenet_v1_coco_2017_11_17')
+detection_graph, category_index = backbone.set_model('ssdlite_mobilenet_v2_coco_2018_05_09')
 
 #object_counting_api.object_counting(input_video, detection_graph, category_index, 0) # for counting all the objects, disabled color prediction
 
@@ -26,8 +26,8 @@ detection_graph, category_index = backbone.set_model('ssd_mobilenet_v1_coco_2017
 
 targeted_objects = "person" # (for counting targeted objects) change it with your targeted objects
 fps = 24 # change it with your input video fps
-width = 854 # change it with your input video width
-height = 480 # change it with your input vide height
+width = 480 # change it with your input video width
+height = 360 # change it with your input vide height
 is_color_recognition_enabled = 0
 
 #object_counting_api.targeted_object_counting(input_video, detection_graph, category_index, is_color_recognition_enabled, targeted_objects, fps, width, height) # targeted objects counting
