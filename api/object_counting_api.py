301aa19a9d72ecf27fd079c7c3b1e453b246b132
ahmetozlu <ahmetozlu93@gmail.com>
2018-10-25 22:21:37 +0300

    sample input image added

--- api/object_counting_api.py
+++ api/object_counting_api.py
@@ -486,7 +486,7 @@ def single_image_object_counting(input_video, detection_graph, category_index, i
         else:
             cv2.putText(input_frame, counting_mode, (10, 35), font, 0.8, (0,255,255),2,cv2.FONT_HERSHEY_SIMPLEX)
         
-        cv2.imshow('object counting',input_frame)        
+        cv2.imshow('tensorflow_object counting_api',input_frame)        
         cv2.waitKey(0)
 
         return counting_mode       
