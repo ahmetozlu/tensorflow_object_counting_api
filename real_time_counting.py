63ad5c8ee9f864c88d766db5b48837a90589d340
ahmetozlu <ahmetozlu93@gmail.com>
2019-04-20 15:24:04 +0300

    tf version checker removed.

--- real_time_counting.py
+++ real_time_counting.py
@@ -11,9 +11,6 @@ import tensorflow as tf
 from utils import backbone
 from api import object_counting_api
 
-if tf.__version__ < '1.4.0':
-  raise ImportError('Please upgrade your tensorflow installation to v1.4.* or later!')
-
 input_video = "./input_images_and_videos/New Office TOUR!  Karlie Kloss.mp4"
 
 # By default I use an "SSD with Mobilenet" model here. See the detection model zoo (https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/detection_model_zoo.md) for a list of other models that can be run out-of-the-box with varying speeds and accuracies.
