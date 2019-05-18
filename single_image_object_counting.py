16d63c317964fccf1dd3c0d9ae0771b30122a93b
ahmetozlu <ahmetozlu93@gmail.com>
2019-05-18 15:17:44 +0300

    ssd model upgraded

--- single_image_object_counting.py
+++ single_image_object_counting.py
@@ -14,7 +14,7 @@ from api import object_counting_api
 input_video = "./input_images_and_videos/sample_input_image.jpg"
 
 # By default I use an "SSD with Mobilenet" model here. See the detection model zoo (https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/detection_model_zoo.md) for a list of other models that can be run out-of-the-box with varying speeds and accuracies.
-detection_graph, category_index = backbone.set_model('ssd_mobilenet_v1_coco_2017_11_17')
+detection_graph, category_index = backbone.set_model('ssd_mobilenet_v1_coco_2018_01_28')
 
 fps = 30 # change it with your input video fps
 width = 626 # change it with your input video width
