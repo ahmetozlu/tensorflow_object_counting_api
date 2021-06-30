88df50f98e0f4d105071bd46845ea30e031a1558
haziyevv <ferid.heziyev@gmail.com>
2021-06-30 15:33:41 +0400

    Fixed the compatibility problems caused by tensorflow version. Just replaced **tensorflow** imports with **tensorflow.compat.v1** and **tf.disable_v2_behavior** thats all.

--- utils/label_map_util.py
+++ utils/label_map_util.py
@@ -17,7 +17,9 @@
 
 import logging
 
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+
 from google.protobuf import text_format
 from protos import string_int_label_map_pb2
 
